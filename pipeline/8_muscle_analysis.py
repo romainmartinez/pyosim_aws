239c228bc2bc5c6d5ed86cd5c583ef0a76dde702
romain <martinez.staps@gmail.com>
2019-04-11 14:55:58 -0400

    muscle analysis

--- pipeline/8_muscle_analysis.py
+++ pipeline/8_muscle_analysis.py
@@ -4,6 +4,7 @@ Muscle analysis
 import yaml
 from pyosim import Conf
 from pyosim import MuscleAnalysis
+import pandas as pd
 
 aws_conf = yaml.safe_load(open("../conf.yml"))
 local_or_distant = "distant" if aws_conf["distant_id"]["enable"] else "local"
@@ -14,15 +15,53 @@ conf.check_confs()
 
 model_names = ["wu"]
 
+blacklist = [
+    "wu_AnnSF6H2_1",
+    "wu_AnnSF6H2_3",
+    "wu_SteBF6H2_2",
+    "wu_JawRH12H4_1",
+    "wu_JawRH12H3_3",
+    "wu_JawRH12H3_2",
+    "wu_JawRH12H2_1",
+    "wu_JawRH12H1_3",
+    "wu_NemKH6H2_3",
+    "wu_NemKH6H1_3",
+    "wu_NemKH6H1_2",
+    "wu_NemKH6H1_1",
+    "wu_NemKH18H2_3",
+    "wu_NemKH12H2_2",
+    "wu_NemKH12H1_3",
+    "wu_NemKH12H1_2",
+    "wu_GeoAH6H1_2",
+    "wu_GeoAH18H2_3",
+    "wu_GeoAH18H1_2",
+    "wu_DavOH12H2_3",
+    "wu_PatMH12H2_2",
+]
+
+# append blacklist with verifications
+verif_file = conf.project_path / "verification.csv"
+try:
+    blacklist.extend(
+        pd.read_csv(verif_file, index_col=[0]).query("tag > 1")["trial"].tolist()
+    )
+except FileNotFoundError:
+    print(f"{verif_file} not found.")
+
 for iparticipant in participants:
     print(f"\nparticipant: {iparticipant}")
 
-    trials = [
-        ifile
-        for ifile in (conf.project_path / iparticipant / "1_inverse_kinematic").glob(
-            "*.mot"
+    trials = list(
+        filter(
+            None,
+            [
+                ifile if ifile.stem not in blacklist else ""
+                for ifile in (
+                    conf.project_path / iparticipant / "1_inverse_kinematic"
+            ).glob("*.mot")
+            ],
         )
-    ]
+    )
 
     for imodel in model_names:
         path_kwargs = {
@@ -30,7 +69,7 @@ for iparticipant in participants:
             "xml_input": f"{(conf.project_path / '_templates' / imodel).resolve()}_ma.xml",
             "xml_output": f"{(conf.project_path / iparticipant / '_xml' / imodel).resolve()}_ma.xml",
             "xml_forces": f"{(conf.project_path / '_templates' / 'forces_sensor.xml').resolve()}",
-            "xml_actuators": f"{(conf.project_path / f'{imodel}_actuators.xml').resolve()}",
+            "xml_actuators": f"{(conf.project_path / '_templates' / f'{imodel}_actuators.xml').resolve()}",
             "ext_forces_dir": f"{(conf.project_path / iparticipant / '0_forces').resolve()}",
             "sto_output": f"{(conf.project_path / iparticipant / '4_muscle_analysis').resolve()}",
             # "enforce_analysis": True,
